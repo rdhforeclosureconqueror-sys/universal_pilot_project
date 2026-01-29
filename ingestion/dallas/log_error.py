1a157fed7125577992a0b3644e9c4a4a772d7580
rdhforeclosureconqueror-sys <rdhforeclosureconqueror@gmail.com>
2026-01-29 17:53:20 -0600

    Add Dallas County PDF ingestion adapter

---
+++ ingestion/dallas/log_error.py
@@ -0,0 +1,7 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def log_error(row: list, exception: Exception) -> None:
+    logger.warning("Dallas PDF row rejected", extra={"row": row, "error": str(exception)})
